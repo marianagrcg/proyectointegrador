2bae4403ce5e7ef8c27749ebd91f16a27ad1c5fd
mareneerms <119461320+mareneerms@users.noreply.github.com>
2022-11-29 22:34:28 -0700

    Update Proveedor.cpp

--- Proveedor.cpp
+++ Proveedor.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
 #include <string>
-#include "Proveedor.h"
-#include "Producto.h"
+#include "Proveedor.hpp"
+#include "Producto.hpp"
 using namespace std;
 
 Proveedor::Proveedor()
 {
-	nombreProveedor = "";
-	stock = 0;
+    nombreProveedor = "";
+    stock = 0;
 
 };
 Proveedor::Proveedor(string cNombreProveedor, Producto cProducto, int cStock)
 {
-	nombreProveedor = cNombreProveedor;
-	producto = cProducto;
-	stock = cStock;
+    nombreProveedor = cNombreProveedor;
+    producto = cProducto;
+    stock = cStock;
 };
 
 //GETTERS
 string Proveedor::getNombreProveedor()
 {
-	return nombreProveedor;
+    return nombreProveedor;
 };
 Producto Proveedor::getProducto()
 {
-	return producto;
+    return producto;
 };
 int Proveedor::getStock()
 {
-	return stock;
+    return stock;
 };
 
 //SETTERS
 void Proveedor::setNombreProveedor(string cNombreProveedor)
 {
-	nombreProveedor = cNombreProveedor;
+    nombreProveedor = cNombreProveedor;
 };
 void Proveedor::setProducto(Producto cProducto)
 {
-	producto = cProducto;
+    producto = cProducto;
 };
 void Proveedor::setStock(int cStock)
 {
-	stock = cStock;
+    stock = cStock;
 };
 
 //FUNCIONALIDADES
-void Proveedor::imprimeStock(Producto)
+void Proveedor::imprimeStock(Producto p)
 {
-    cout << "El stock es: " << stock <<endl;
-    cout << "El precio unitario es: " << producto.getPrecioProducto() << endl;
+    
+    cout << "El stock de "<< nombreProveedor << " es: " << stock <<endl;
+    cout << "El precio unitario por pieza es: " <<"$"<< p.getPrecioProducto() << endl;
 };
 
 //DESTRUCTOR
