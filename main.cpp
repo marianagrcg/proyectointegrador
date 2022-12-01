0878226e8162206656c295dd06cd93eda6e66a2d
Hector Gutierrez <114173304+HectorGtz27@users.noreply.github.com>
2022-11-30 20:57:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ using namespace std;
 
 int main()
 {
-	
+
 	Aplicacion app("ONLINESHOP");
 	int option = 0;
 
 	cout << "Ingresa la opcion que deseas realizar \n 1- Agregar Preveedor. \n 2- Agregar Comprador. \n 3-Para salir" << endl;
 	cin >> option;
-	
-	
+
+
 	while (option == 1 || option == 2)
 	{
 		if (option == 1) {
@@ -42,11 +42,12 @@ int main()
 			cin >> stock;
 
 			app.setProveedor(nombreProveedor, producto, stock);
+
 			cout << "Ingresa la opcion que deseas realizar \n 1- Agregar Preveedor. \n 2- Agregar Comprador. \n 3-Para salir" << endl;
 			cin >> option;
 
 		}
-		
+
 
 		else {
 			//DATOS COMPRADOR
@@ -67,29 +68,26 @@ int main()
 			cout << "Ingresa la opcion que deseas realizar \n 1- Agregar Preveedor. \n 2- Agregar Comprador. \n 3-Para salir" << endl;
 			cin >> option;
 		}
-			
 
+
+		app.printproveedor();
+		app.printcomprador();
+
+		return 0;
 	}
-	
-	
 
 
-	
-	/*vector<Proveedor> proveedores;
 
-	Producto producto1("Vasos", 500);
 
-	Proveedor proveedor1("Juanito", producto1, 5);
 
-	//Nombre   //proveedor  //saldo //cantidad  
-	Comprador comprador1("Daniela", proveedor1, 10000, 4);
-
-	proveedor1[0].imprimeStock(producto1);
-
-	comprador1[0].comprarStock(proveedor1);
-
-	comprador1[0].saldoActualizado(producto1);
-
-	comprador1[0].actualizarStock(proveedor1);*/
+	//vector<Proveedor> proveedores;
+	//Producto producto1("Vasos", 500);
+	//Proveedor proveedor1("Juanito", producto1, 5);
+	//Nombre   //proveedor  //saldo //cantidad
+	//Comprador comprador1("Daniela", proveedor1, 10000, 4);
+	//proveedor1[0].imprimeStock(producto1);
+	//comprador1[0].comprarStock(proveedor1);
+	//comprador1[0].saldoActualizado(producto1);
+	//comprador1[0].actualizarStock(proveedor1);
 
 }
