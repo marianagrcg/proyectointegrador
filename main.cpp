02f4c381daf714874364a8d532e0e6027bd7aa91
Hector Gutierrez <114173304+HectorGtz27@users.noreply.github.com>
2022-11-30 18:41:17 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Proveedor.hpp"
-#include "Comprador.hpp"
-#include "Producto.hpp"
+#include "Proveedor.h"
+#include "Comprador.h"
+#include "Producto.h"
 using namespace std;
 
 
 int main()
 {
     //vector<Proveedor> proveedores;
-    
-    Producto producto1("Vasos",120);
-    
+
+    Producto producto1("Vasos", 500);
+
     Proveedor proveedor1("Juanito", producto1, 5);
-    
-    Comprador comprador1("Daniela",proveedor1,1000,0);
-    
+
+                        //Nombre   //proveedor  //saldo //cantidad  
+    Comprador comprador1("Daniela", proveedor1, 10000, 4);
+
     proveedor1.imprimeStock(producto1);
-    
+
     comprador1.comprarStock(proveedor1);
-    
+
     comprador1.saldoActualizado(producto1);
-  
+
+    comprador1.actualizarStock(proveedor1);
 }
