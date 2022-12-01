316e963994cc81c6e76999d6bfcaa686bda3717a
Hector Gutierrez <114173304+HectorGtz27@users.noreply.github.com>
2022-11-30 20:58:03 -0700

    Update Aplicacion.cpp

--- Aplicacion.cpp
+++ Aplicacion.cpp
@@ -1,13 +1,16 @@
+#ifndef Aplicacion_h
+#define Aplicacion_h
+#include "Producto.h"
 #include <iostream>
 #include <string>
 #include <vector>
 #include "Proveedor.h"
 #include "Aplicacion.h"
+#include "ostream"
 using namespace std;
 
 //COSNTRUCTOR
 
-Aplicacion::~Aplicacion() {};
 
 Aplicacion::Aplicacion()
 {
@@ -22,7 +25,7 @@ Aplicacion::Aplicacion(string cName)
 string Aplicacion::getName()
 {
 	return name;
-}
+};
 void Aplicacion::setName(string cName)
 {
 	name = cName;
@@ -47,3 +50,33 @@ void Aplicacion::setComprador(string cNombreComprador, Proveedor cProducto, doub
 	Comprador comp(cNombreComprador, cProducto, cSaldo, cCantidad);
 	compradores.push_back(comp);
 };
+
+void Aplicacion::printproveedor() {
+	cout << endl << "Empleados -------" << endl;
+	for (int i = 0; i < proveedores.size(); i++) {
+		cout << proveedores[i].getNombreProveedor() << " " << endl;
+		//proveedores[i].getProducto() << " " << 
+		//proveedores[i].getStock() << " " << endl;
+
+
+	}
+
+
+};
+
+void Aplicacion::printcomprador() {
+	cout << endl << "Empleados -------" << endl;
+	for (int i = 0; i < compradores.size(); i++) {
+		cout << compradores[i].getNombreComprador() << " " << endl;
+		//compradores[i].getProducto() << " " << 
+		//compradores[i].getSaldo() << " " << 
+		//compradores[i].getCantidad() << endl;
+
+
+	}
+
+
+};
+#endif
+
+Aplicacion::~Aplicacion() {};
