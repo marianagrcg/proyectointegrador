c6e5d202e4b18b29480b4fd7af968a71a0e781c6
danielagallardoc <119468005+danielagallardoc@users.noreply.github.com>
2022-11-30 23:30:40 -0700

    Update Producto.h

--- Producto.h
+++ Producto.h
@@ -1,27 +1,29 @@
 #pragma once
+
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Producto
 {
-	private:
-		string nombreProducto;
-		double precioProducto;
-	public:
-		//CONSTRUCTOR
-		Producto();
-		Producto(string cNombreProducto, double cPrecioProducto);
+    private:
+        string nombreProducto;
+        double precioProducto;
+    public:
+        //CONSTRUCTOR
+        Producto();
+        Producto(string cNombreProducto, double cPrecioProducto);
 
-		//GETTERS
-		string getNombreProducto();
-		double getPrecioProducto();
+        //GETTERS
+        string getNombreProducto();
+        double getPrecioProducto();
 
-		//SETTERS
-		void setNombreProducto(string cNombreProducto);
-		void setPrecioProducto(double cPrecioProducto);
+        //SETTERS
+        void setNombreProducto(string cNombreProducto);
+        void setPrecioProducto(double cPrecioProducto);
 
 
-		//DESTRUCTOR
-		~Producto();
+        //DESTRUCTOR
+        ~Producto();
 };
