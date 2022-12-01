6345f97db39eb73befbc7dda1667bb186ddfd1cf
marianagrcg <81926038+marianagrcg@users.noreply.github.com>
2022-11-30 20:10:09 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,92 @@
 #include "Proveedor.h"
 #include "Comprador.h"
 #include "Producto.h"
+#include "Aplicacion.h"
 using namespace std;
 
 
 int main()
 {
-    //vector<Proveedor> proveedores;
+	
+	Aplicacion app("ONLINESHOP");
+	int option = 0;
 
-    Producto producto1("Vasos", 500);
+	cout << "Ingresa la opcion que deseas realizar \n 1- Agregar Preveedor. \n 2- Agregar Comprador. \n 3-Para salir" << endl;
+	cin >> option;
+	
+	
+	while (option == 1 || option == 2)
+	{
+		if (option == 1) {
+			//DATOS PROVEEDOR
+			cout << "Ingresa el nombre del Proveedor: ";
+			string nombreProveedor;
+			cin >> nombreProveedor;
 
-    Proveedor proveedor1("Juanito", producto1, 5);
+			//Producto
+			cout << "Ingresa nombre producto: ";
+			string nombreProducto;
+			cin >> nombreProducto;
 
-                        //Nombre   //proveedor  //saldo //cantidad  
-    Comprador comprador1("Daniela", proveedor1, 10000, 4);
+			cout << "Ingresa el precio del producto: ";
+			double precioProducto;
+			cin >> precioProducto;
 
-    proveedor1.imprimeStock(producto1);
+			Producto producto(nombreProducto, precioProducto);
 
-    comprador1.comprarStock(proveedor1);
+			cout << "Ingresa stock: ";
+			int stock;
+			cin >> stock;
 
-    comprador1.saldoActualizado(producto1);
+			app.setProveedor(nombreProveedor, producto, stock);
+			cout << "Ingresa la opcion que deseas realizar \n 1- Agregar Preveedor. \n 2- Agregar Comprador. \n 3-Para salir" << endl;
+			cin >> option;
 
-    comprador1.actualizarStock(proveedor1);
+		}
+		
+
+		else {
+			//DATOS COMPRADOR
+			cout << "Ingresa el nombre del Comprador: ";
+			string nombreComprador;
+			cin >> nombreComprador;
+
+			cout << "Ingresa saldo: ";
+			double saldo;
+			cin >> saldo;
+
+			cout << "Ingresa la cantidad a comprar: ";
+			int cantidad;
+			cin >> cantidad;
+
+			app.setComprador(nombreComprador, app.getProveedor(), saldo, cantidad);
+
+			cout << "Ingresa la opcion que deseas realizar \n 1- Agregar Preveedor. \n 2- Agregar Comprador. \n 3-Para salir" << endl;
+			cin >> option;
+		}
+			
+
+	}
+	
+	
+
+
+	
+	/*vector<Proveedor> proveedores;
+
+	Producto producto1("Vasos", 500);
+
+	Proveedor proveedor1("Juanito", producto1, 5);
+
+	//Nombre   //proveedor  //saldo //cantidad  
+	Comprador comprador1("Daniela", proveedor1, 10000, 4);
+
+	proveedor1[0].imprimeStock(producto1);
+
+	comprador1[0].comprarStock(proveedor1);
+
+	comprador1[0].saldoActualizado(producto1);
+
+	comprador1[0].actualizarStock(proveedor1);*/
+
 }
