ca100516da17cc9d326d5088170638c6824c7024
Hector Gutierrez <114173304+HectorGtz27@users.noreply.github.com>
2022-11-30 18:40:13 -0700

    Update Comprador.cpp

--- Comprador.cpp
+++ Comprador.cpp
@@ -1,6 +1,6 @@
-#include "Comprador.hpp"
-#include "Proveedor.hpp"
-#include "Producto.hpp"
+#include "Comprador.h"
+#include "Proveedor.h"
+#include "Producto.h"
 #include <string>
 #include <iostream>
 using namespace std;
@@ -8,12 +8,16 @@ using namespace std;
 Comprador::Comprador()
 {
     nombreComprador = "";
+    saldo = 0;
+    cantidad = 0;
 };
 
 Comprador::Comprador(string cNombreComprador, Proveedor cProducto, double cSaldo, int cCantidad)
 {
     nombreComprador = cNombreComprador;
     producto = cProducto;
+    saldo = cSaldo;
+    cantidad = cCantidad;
 };
 
 //GETTER
@@ -26,6 +30,17 @@ Proveedor Comprador::getProducto()
     return producto;
 };
 
+double Comprador::getSaldo() {
+
+    return saldo;
+
+};
+
+int Comprador::getCantidad() {
+    
+    return cantidad;
+};
+
 //SETTER
 void Comprador::setNombreComprador(string cNombreComprador)
 {
@@ -35,6 +50,16 @@ void Comprador::setProducto(Proveedor cProducto)
 {
     producto = cProducto;
 }
+
+
+void Comprador::setCantidad(int cCantidad) {
+
+    cantidad = cCantidad;
+};
+
+
+// Funcionalidades
+
 int Comprador::comprarStock(Proveedor p)
 {
     //Producto producto1("Vasos", 200);
@@ -42,18 +67,30 @@ int Comprador::comprarStock(Proveedor p)
     cout << "Cantidad a comprar: ";
     cin >> cantidad;
 
-    int a= p.getStock();
-    if (cantidad <= a){
+    int a = p.getStock();
+    if (cantidad <= a) {
 
-        cout << "Compra realizada con exito"<<endl;
-        return a=(p.getStock() - cantidad);
-    }else{
-            cout << "No se pudo realizar la compra"<<endl;}
-            return a;
+        cout << "Compra realizada con exito" << endl;
+        return a = (p.getStock() - cantidad);
+    }
+    else {
+        cout << "No se pudo realizar la compra" << endl;
+    }
+    return a;
 };
 
-double Comprador:: saldoActualizado(Producto g){
-    return (saldo - (cantidad * g.getPrecioProducto()));
+void Comprador::saldoActualizado( Producto g) {
+    double saldoNuevo;
+    saldoNuevo = (saldo - (cantidad * g.getPrecioProducto()));
+    cout << "Saldo restante: $" << saldoNuevo << endl;
+        
 };
+
+void Comprador::actualizarStock(Proveedor p) {
+    cout << "Piezas compradas: " << cantidad << endl;
+    cout << "Actualizacion de stock: " << (p.getStock() - cantidad) << endl;
+};
+
+
+
 Comprador::~Comprador() {};
-
